4d12a6663600cac526e8f00ca0fd98401b2cd26c
Tarasov Andrey <TarasovAE@sibedge.com>
2020-07-06 20:40:28 +0300

    Updates

---
+++ precache-manifest.046b89c6367adacbd90f6e2532e6017b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ad1f544946db74b37bc9c4fbd1671ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c122cbb0e2c217169a2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "95a62b4d7bb6ec38c3fb",
+    "url": "/static/js/2.5b2e7e02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5b2e7e02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c122cbb0e2c217169a2",
+    "url": "/static/js/main.2d6a20f1.chunk.js"
+  },
+  {
+    "revision": "96b22b40f07f7bc10137",
+    "url": "/static/js/runtime-main.54c15204.js"
+  }
+]);
